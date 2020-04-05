ca9938a67344ac192b8c7ad0ddd4a6dd788453f6
Jinqi Li <jxl2047@miami.edu>
2020-04-05 17:04:00 -0400

    Initial Commit

---
+++ ClimateProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClimateProject
+//
+//  Created by Jinqi Li on 4/5/20.
+//  Copyright © 2020 Jinqi Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
