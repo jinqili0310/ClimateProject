5f054958abca56e470e0a22a4a076921afaeb069
Jinqi Li <jxl2047@miami.edu>
2020-06-05 22:28:58 -0400

    country page

--- ClimateProject/Views/CountryViewController.swift
+++ ClimateProject/Views/CountryViewController.swift
@@ -9,38 +9,90 @@
 import UIKit
 import Firebase
 
-class CountryViewController: UIViewController {
+class CountryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var nameText: String!
-    @IBOutlet weak var countryField: UITextField!
     var documentID: String!
+    
+    @IBOutlet weak var countryButton: UIButton!
+    var countryList: [String] = [String]()
+    let cellReuseIdentifier = "cell"
+    let cellSpacingHeight: CGFloat = 0
+    @IBOutlet weak var dropdownView: UIView!
+    @IBOutlet var countryView: UITableView! {
+        didSet {
+            countryView.dataSource = self
+        }
+    }
+    var countryTapped: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-// MARK: - Keyboard Issues
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        self.view.addGestureRecognizer(tapGesture)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        dropdownView.isHidden = true
+        self.countryView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        countryView.delegate = self
+        countryView.dataSource = self
+        countryList = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China, People's Republic of", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"]
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
-        // Do any additional setup after loading the view.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Set the spacing between sections
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return cellSpacingHeight
+    }
+
+    // Make the background color show through
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countryList.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath as IndexPath) as UITableViewCell
+        cell.textLabel?.text = countryList[indexPath.row]
+        cell.textLabel?.font = UIFont(name:"Montserrat-medium", size:25)
+        // add border and color
+        cell.backgroundColor = UIColor.white
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 2
+        cell.layer.cornerRadius = 25
+        cell.clipsToBounds = true
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped \(countryList[indexPath.row]).")
+        countryButton.titleLabel?.text = countryList[indexPath.row]
+        countryTapped = countryList[indexPath.row]
+        dropdownView.isHidden = true
+    }
+    
+    @IBAction func countryPressed(_ sender: UIButton) {
+        dropdownView.isHidden = false
     }
     
 // MARK: - Save Name
         
-        @IBAction func countrySaved(_ sender: UIButton) {
-            let countryInput = countryField.text!
-            let db = Firestore.firestore()
-            
-            if documentID != nil {
-                db.collection("users").document(documentID!).setData(["country of origin": countryInput], merge: true)
+    @IBAction func countrySaved(_ sender: UIButton) {
+        let db = Firestore.firestore()
+        if documentID != nil {
+            db.collection("users").document(documentID!).setData(["country of origin": countryTapped!], merge: true)
 
-                performSegue(withIdentifier: "goToDemoEnd", sender: self)
-            }
-            
+            performSegue(withIdentifier: "goToDemoEnd", sender: self)
         }
+            
+    }
         
 // MARK: - Navigation
         
@@ -51,7 +103,7 @@ class CountryViewController: UIViewController {
                 return false
             }
         }
-        
+
         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             if segue.identifier == "goToDemoEnd" {
                 let demoendVC = segue.destination as! DemoEndViewController
@@ -60,33 +112,6 @@ class CountryViewController: UIViewController {
             }
         }
         
-// MARK: - Keyboard Issues
-
-        @objc func keyboardWillShow(notification: NSNotification) {
-                print(self.view.frame.origin.y)
-            let textSize = countryField.frame.origin.y + countryField.frame.height
-            print(textSize)
-                if let keyboardSize = (notification.userInfo? [UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-                    if self.view.frame.origin.y == 0 {
-                        if (keyboardSize.height+textSize)>self.view.frame.height {
-                            self.view.frame.origin.y += self.view.frame.height - (keyboardSize.height + textSize)
-                        }
-                    }
-                    print(self.view.frame.origin.y)
-                }
-            }
-            
-            @objc func keyboardWillHide(notification: NSNotification) {
-                if self.view.frame.origin.y != 0 {
-                    self.view.frame.origin.y = 0
-                }
-                print(self.view.frame.origin.y)
-            }
-            
-            @objc func dismissKeyboard() {
-                view.endEditing(true)
-            }
-
     /*
     // MARK: - Navigation
 
